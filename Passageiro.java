be9ab278571d4d0e28b14ba8415a17e165a234e5
Hiram Artnak Martins <hiram.martins@edu.pucrs.br>
2020-03-29 20:16:56 -0300

    Added Passageiro

---
+++ Passageiro.java
@@ -0,0 +1,24 @@
+public class Passageiro{
+    private String CPF;
+    private String nome;
+    private FormaPagamento formaPagamento;
+    private String nroCartao;
+
+    public String getCPF(){
+        return CPF;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public FormaPagamento getFormaPagamento(){
+        return formaPagamento;
+    }
+
+    public String getNroCartao(){
+        return nroCartao;
+    }
+
+
+}
