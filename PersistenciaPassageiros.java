d7b388956664689ee0d77bba4d19ea5f0898a494
Pedro Carlucci <pkcarlucci@gmail.com>
2020-03-29 21:30:20 -0300

    Criando persistenciapassageiros

---
+++ PersistenciaPassageiros.java
@@ -0,0 +1,16 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class PersistenciaPassageiros{
+    private static final String SAMPLE_CSV_FILE_PATH = "passageiros.dat";
+    public PersistenciaPassageiros() throws IOException{
+        
+    }
+
+}
