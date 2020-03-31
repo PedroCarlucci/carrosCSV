86bec3fb93d5383b3d97fa7a5e173525baac4df4
Pedro Carlucci <pkcarlucci@gmail.com>
2020-03-30 22:57:44 -0300

    PersistenciaPassageiros construtor

--- PersistenciaPassageiros.java
+++ PersistenciaPassageiros.java
@@ -5,12 +5,25 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class PersistenciaPassageiros{
-    private static final String SAMPLE_CSV_FILE_PATH = "passageiros.dat";
-    public PersistenciaPassageiros() throws IOException{
-        
+    private static String FILE_PATH = "passageiros.dat";
+    private static List<Passageiro> listaPassageiros;
+    
+    public PersistenciaPassageiros(String path, List<Passageiro> listaPassageiros){
+        this.FILE_PATH = path;
+        this.listaPassageiros = listaPassageiros;
+
     }
 
+    public List<Passageiro> carregaPassageiros() throws IOException{
+        LinkedList<Motorista> listaPassageiro = new LinkedList<Motorista>();
+        try (Reader reader = Files.newBufferedReader(Paths.get(FILE_PATH));
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT); ){
+            
+        }
+    }
 }
