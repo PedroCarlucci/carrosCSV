02e9b1b0645a1b2f6d98c6be524530c374cd5026
Hiram Artnak Martins <hiram.martins@edu.pucrs.br>
2020-03-30 22:12:49 -0300

    standardized list types

--- PersistenciaMotoristas.java
+++ PersistenciaMotoristas.java
@@ -5,25 +5,23 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 public class PersistenciaMotoristas
 {
     private static final String SAMPLE_CSV_FILE_PATH = "motorista.dat";
-    public PersistenciaMotoristas() throws IOException
+    public List<Motorista> carregaMotoristas() throws IOException
     {
-        ArrayList<Motorista> listamotorista = new ArrayList<Motorista>(11);
-        try 
-        (
+        LinkedList<Motorista> listaMotorista = new LinkedList<Motorista>();
             Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
-        ){
             
             for (CSVRecord csvRecord : csvParser)
             {
-                listamotorista.add(new Motorista(csvRecord.get(0),csvRecord.get(1),csvRecord.get(2),FormaPagamento.valueOf(csvRecord.get(3))));
-
 
             }
+
+            return listaMotorista;
         }
     }
 }
--- PersistenciaVeiculos.java
+++ PersistenciaVeiculos.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.List;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -5,28 +7,23 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-public class PersistenciaVeiculos
-{
+public class PersistenciaVeiculos{
     private static final String SAMPLE_CSV_FILE_PATH = "veiculos.dat";
     
-    public PersistenciaVeiculos()
-    {
-        try 
-        (
-            Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
-            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
-        ){
-            for (CSVRecord csvRecord : csvParser)
-            {
-                String placa = csvRecord.get(0);
-                String marca = csvRecord.get(1);
-                String cor = csvRecord.get(2);
-                String categoria = csvRecord.get(3);
+    public List<Veiculo> carregaVeiculos() throws IOException {
 
+        List<Veiculo> listaVeiculo = new LinkedList<Veiculo>();
 
+        Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
 
-
-            }
+        for (CSVRecord csvRecord : csvParser){
+            String placa = csvRecord.get(0);
+            String marca = csvRecord.get(1);
+            String cor = csvRecord.get(2);
+            String categoria = csvRecord.get(3);
         }
-    }
+        return listaVeiculo;
+     }
+
 }
