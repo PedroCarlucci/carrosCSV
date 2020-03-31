c3cabaf896e2338b8b88a94a82f79e2b726ebbc3
Hiram Artnak Martins <hiram.martins@edu.pucrs.br>
2020-03-30 22:15:01 -0300

    Fixed weird identation

--- PersistenciaMotoristas.java
+++ PersistenciaMotoristas.java
@@ -10,18 +10,16 @@ import java.util.List;
 public class PersistenciaMotoristas
 {
     private static final String SAMPLE_CSV_FILE_PATH = "motorista.dat";
-    public List<Motorista> carregaMotoristas() throws IOException
-    {
+    
+    public List<Motorista> carregaMotoristas() throws IOException{
+
         LinkedList<Motorista> listaMotorista = new LinkedList<Motorista>();
-            Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
-            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
-            
-            for (CSVRecord csvRecord : csvParser)
-            {
+        Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);    
+        for (CSVRecord csvRecord : csvParser){
 
-            }
-
-            return listaMotorista;
         }
+
+        return listaMotorista;
     }
 }
