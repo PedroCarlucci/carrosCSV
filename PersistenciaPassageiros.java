940a3c50aebde87f38bce210bd92c3eb25ab6613
Pedro Carlucci <pkcarlucci@gmail.com>
2020-03-30 23:07:24 -0300

    metodo carregaPassageiros

--- PersistenciaPassageiros.java
+++ PersistenciaPassageiros.java
@@ -10,8 +10,8 @@ import java.util.List;
 
 
 public class PersistenciaPassageiros{
-    private static String FILE_PATH = "passageiros.dat";
-    private static List<Passageiro> listaPassageiros;
+    private String FILE_PATH = "passageiros.dat";
+    private List<Passageiro> listaPassageiros;
     
     public PersistenciaPassageiros(String path, List<Passageiro> listaPassageiros){
         this.FILE_PATH = path;
@@ -21,9 +21,17 @@ public class PersistenciaPassageiros{
 
     public List<Passageiro> carregaPassageiros() throws IOException{
         LinkedList<Motorista> listaPassageiro = new LinkedList<Motorista>();
-        try (Reader reader = Files.newBufferedReader(Paths.get(FILE_PATH));
-        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT); ){
-            
+        try (
+            Reader reader = Files.newBufferedReader(Paths.get(FILE_PATH));
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+        ){
+            for (CSVRecord csvRecord : csvParser){
+                String CPF = csvRecord.get(0);
+                String nome = csvRecord.get(1);
+                FormaPagamento formaPagamento  = FormaPagamento.valueOf(csvRecord.get(2));
+                String nroCartao = csvRecord.get(3);
+            }
         }
+        return listaPassageiros;
     }
 }
